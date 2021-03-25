6a7d48d24e4e7eded861cf7e0b0cbcba7157e851
CJcool06 <cjcool06@yahoo.com.au>
2021-03-25 12:11:45 +1030

    3.4

--- task3.js
+++ task3.js
@@ -4,9 +4,29 @@ function increaseCount() {
     document.getElementById("mcount").innerHTML = "<div>" + count + "</div>";
 }
 
+var data = [];
 function postText() {
-    document.getElementById("posts").innerHTML = "<div class='post-time'>" + new Date() + "</div>";
-    document.getElementById("posts").innerHTML += "<div class='post-content'><br />" + document.getElementById("text_area").value + "</div>";
+    document.getElementById("posts").innerHTML = "";
+
+    var colors = document.getElementsByName("color");
+	var color = "black";
+
+	for (var i = 0; i < colors.length; i++) {
+		if (colors[i].checked) {
+			color = colors[i].value;
+			break;
+		}
+	}
+
+    var quantity = document.getElementById("quantity").value;
+    for (var i = 0; i < quantity; i++) {
+        data.push("<div class='post-time'>" + new Date() + "</div><div class='post-content' style='color: " + color + "';><br />" + document.getElementById("text_area").value + "</div>");
+    }
+
+    for (var i = 0; i < data.length; i++) {
+        document.getElementById("posts").innerHTML += data[i];
+        document.getElementById("posts").innerHTML += "<br />";
+    }
 }
 
 function changeMenuScreen() {
