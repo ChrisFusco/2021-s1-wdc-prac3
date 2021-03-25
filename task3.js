0d963c0c2f5ab2e6fc4f1c27d4f0ca5e91f1df02
CJcool06 <cjcool06@yahoo.com.au>
2021-03-25 11:34:02 +1030

    task 3.2

--- task3.js
+++ task3.js
@@ -1,5 +1,10 @@
 var count = 0;
 function increaseCount() {
     count++;
-    document.getElementById("mcount").innerHTML = count;
+    document.getElementById("mcount").innerHTML = "<div>" + count + "</div>";
+}
+
+function postText() {
+    document.getElementById("posts").innerHTML = "<div class='post-time'>" + Date() + "</div>";
+    document.getElementById("posts").innerHTML += "<div><br />" + document.getElementById("text_area").innerHTML + "</div>";
 }
