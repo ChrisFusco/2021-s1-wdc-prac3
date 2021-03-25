956b4284119ec3916ab4cbfe8c3d3e5173a12c88
CJcool06 <cjcool06@yahoo.com.au>
2021-03-25 11:48:37 +1030

    task 3.3

--- task3.js
+++ task3.js
@@ -8,3 +8,17 @@ function postText() {
     document.getElementById("posts").innerHTML = "<div class='post-time'>" + new Date() + "</div>";
     document.getElementById("posts").innerHTML += "<div class='post-content'><br />" + document.getElementById("text_area").value + "</div>";
 }
+
+function changeMenuScreen() {
+    var main = document.getElementById("main");
+    var menu = document.getElementById("menu");
+
+    if (menu.style.display === "block") {
+        main.style.display = "block";
+        menu.style.display = "none";
+    }
+    else {
+        main.style.display = "none";
+        menu.style.display = "block";
+    }
+}
